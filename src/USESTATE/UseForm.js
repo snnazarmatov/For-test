ac086c4fe203e7870928f4f97370f202acd0fc55
Esen Nazarmatov <snnazarmatov@gmail.com>
2021-10-11 22:06:45 +0600

    complete hw-for-chicking

--- src/USESTATE/UseForm.js
+++ src/USESTATE/UseForm.js
@@ -4,35 +4,10 @@ import "../USESTATE/UseForm.css"
 
 function UseForm(){
 
-    const [value, setValue] = useState("")
-    const [name, setName] = useState("Esen")
-
-    const handleInput = (e) => {
-        setValue(e.target.value)
-
-    }
-
-    const updateName = (e) => {
-        e.preventDefault()
-        setName(value)
-        setValue("")
-    }
-
 
     return(
         <div className="box">
-            <h1>Hello <span>{name}</span>!</h1>
-            <form>
-                <div>
-                    <label for="name-1">Update Name</label>
-                <div>
-                    <input type="text" value={value} onChange={handleInput}name="name-1"/>
-                </div>
-                </div>
-                <div>
-                    <button className='btn' onClick={updateName}>save</button>
-                </div>
-            </form>
+            <h1>hello everyone how are you</h1>
         </div>
     )
 }
